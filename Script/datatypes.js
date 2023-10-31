4b59723cec099cd03fa15788f137dc785f3be137
GuadalupeMNAN <2102417@utrivieramaya.edu.mx>
2023-10-31 03:06:48 -0500

    first commit

---
+++ Script/datatypes.js
@@ -0,0 +1,15 @@
+//Los comentaris pueden hacer que el codigo sean mas legible.
+//Este es mi primer codigo JS.
+/*Los comentarios pueden 
+hacer que el codigo
+ sea legible, facil de 
+ reutilizar e informativo.*/
+
+ console.log(typeof 'Descrpcion')
+ console.log(typeof true)
+ console.log(typeof undefined)
+ console.log(typeof null)
+
+//Age
+console.log(myAge=22)
+console.log(yourAge=28)
---
+++ Script/variable.js
@@ -0,0 +1,36 @@
+ //Declare variable y asignelr valores
+
+ var Descripcion="Hoy es viernes";
+ var Verdadero_Falso= true;
+ var indifinido=undefined;
+ var not= null;
+ 
+ //Variables no definida
+
+var semana ="";
+var dia ="";
+var tiempo ="";
+var continente ="";
+
+//Variables definidas
+
+var palabra ="Celeste";
+let numero ="28";
+const variable ="true";
+var segundoNombre ="Monserrat"
+
+//Variables en varias lineas
+
+var nombre ="Guadalupe";
+var apellido ="Nunez";
+var estadoCivil ="Soltera";
+var pais ="Mexico";
+var edad ="22";
+
+//Variales en una sola linea
+var nombre ="Guadalupe", apellido ="Nunez", estadoCivil ="Soltera", pais ="Mexico", edad ="22";
+
+
+//Variable myAge y youAge
+var myAge ="22";
+var yourAge ="28";
